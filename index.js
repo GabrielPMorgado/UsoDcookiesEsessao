22faa36a291ab490dbcff5ca21692c0fb6bd61f4
Gabriel <141271554+GabrielPMorgado@users.noreply.github.com>
2024-05-24 03:38:20 -0300

    att

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import session from 'express-session';
 
 const host = '0.0.0.0';
 const porta = 3000;
@@ -7,15 +8,20 @@ const app = express();
 
 
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(process.cwd(), 'pagina')));
-
-app.use(usuarioEstaAutenticado, express.static(path.join(process.cwd(), 'protegido')));
+app.use(session({
+    secret: 'MinhaChave123',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60 * 15 
+    }
+}));
 
 
 function usuarioEstaAutenticado(requisicao,resposta, next){
-    if(usuarioAutenticado){
+    if(requisicao.session.usuarioAutenticado){
         next();
     }
     else{
@@ -23,6 +29,95 @@ function usuarioEstaAutenticado(requisicao,resposta, next){
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function autenticarUsuario(requisicao,resposta){
+    const usuario = requisicao.body.usuario;
+    const senha = requisicao.body.senha;
+    if(usuario == 'admin' && senha =='123'){
+        requisicao.session.usuarioAutenticado= true;
+        resposta.redirect('/');
+    }
+    else{
+        resposta.write(`<!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Senha invalida</title>
+        </head>
+        <body>
+          <script>
+             alert('Usuário ou senha inválidos');
+             window.location.href = '/login'; // Redireciona para a página de login
+          </script>
+        </body>
+        </html>`);
+        resposta.end();
+
+    }
+
+}
+app.post('/login',autenticarUsuario);
+
+app.get('/login',(req,resp)=>{
+    resp.redirect('/login.html');
+});
+
+app.get('/logout',(req,resp)=>{
+    req.session.destroy();
+    resp.redirect('/login.html');
+});
+
+
+
+app.use(express.static(path.join(process.cwd(), 'pagina')));
+app.use(usuarioEstaAutenticado, express.static(path.join(process.cwd(), 'protegido')));
+
+
+
+
+
+app.post('/cadastrarProduto', usuarioEstaAutenticado, cadastrarProduto);
+app.get('/listaDprodutos',usuarioEstaAutenticado,(req,resp)=>{
+
+    resp.write(`<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>`);
+
+})
+
+
 
 
 app.listen(porta, host, () => {
