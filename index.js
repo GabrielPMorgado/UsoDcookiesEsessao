1e6f2c64d295e2ecaf031df9665edef0493f8d6f
Gabriel <141271554+GabrielPMorgado@users.noreply.github.com>
2024-05-26 12:56:07 -0300

    add

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ import express from 'express';
 import path from 'path';
 import session from 'express-session';
 
+
 import cookieParser from 'cookie-parser';
+
 
 const host = '0.0.0.0';
 const porta = 3000;
 const app = express();
 
+
 let listaUsuarios = [];
 
+
 app.use(express.urlencoded({ extended: true }));
+
 
 app.use(session({
     secret: 'MinH4Ch4v3S3cr3t4', 
@@ -22,8 +27,8 @@ app.use(session({
 }));
 
 
-
 app.use(cookieParser());
+
 
 function usuarioEstaAutenticado(requisicao, resposta, next){
     if (requisicao.session.usuarioAutenticado){
@@ -35,18 +40,17 @@ function usuarioEstaAutenticado(requisicao, resposta, next){
 }
 
 
-
 function cadastrarUsuario(requisicao, resposta) {
-    const codico = requisicao.body.codico;
-    const descricao = requisicao.body.descricao;
-    const precoCusto = requisicao.body.precoCusto;
-    const precoVenda = requisicao.body.precoVenda;
-    const validade = requisicao.body.validade;
-    const estoque = requisicao.body.estoque;
-    const fabricante = requisicao.body.fabricante;
+     const codico = requisicao.body.codico;
+     const descricao = requisicao.body.descricao;
+     const precoCusto = requisicao.body.precoCusto;
+     const precoVenda = requisicao.body.precoVenda;
+     const validade = requisicao.body.validade;
+     const estoque = requisicao.body.estoque;
+     const fabricante = requisicao.body.fabricante;
 
     
-    if (codico && descricao && precoCusto && precoVenda && validade && estoque && fabricante) {
+     if (codico && descricao && precoCusto && precoVenda && validade && estoque && fabricante) {
         listaUsuarios.push({
             codico: codico,
             descricao: descricao,
@@ -56,98 +60,117 @@ function cadastrarUsuario(requisicao, resposta) {
             estoque: estoque,
             fabricante: fabricante,
         });
-        resposta.redirect('/listarUsuarios');
-    } else {
-        resposta.write(`<!DOCTYPE html>
-        <html lang="pt-br">
-        <head>
+
+
+     resposta.redirect('/listarUsuarios');
+     } 
+     else{
+         resposta.write(`<!DOCTYPE html>
+         <html lang="pt-br">
+         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        </head>
-        <body>
-            <form method="POST" action='/cadastrarEmpressa'>
-            <label for="cnpj">CNPJ:</label>
-            <input type="text" id="cnpj" name="cnpj" value="${codico}" required>`);
-        if (codico == "") {
+            <title>Cadastro de Produtos</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+         </head>
+         <body>
+            <h1>Cadastro de Produtos</h1>
+            <form method="POST" action='/cadastrarProduto'>
+                <label for="codigo">Código de Barras:</label>
+                <input type="text" id="codigo" name="codigo" value="${codico}" required>`);
+         if (codico="") {
             resposta.write(`
-                            <div m-2 class="alert alert-danger" role="alert">
-                                Por favor, informe o cnpj.
-                            </div>
-                `);
+                <div class="alert alert-danger" role="alert">
+                    Por favor, informe o código de barras.
+                </div>
+            `);
         }
-        resposta.write(`
-            <label for="razao_social">Razão Social ou Nome do Fornecedor:</label>
-            <input type="text" id="razao_social" name="razao_social" placeholder="Moraes & irmãos Ltda" value="${descricao}" required>`);
-        if (descricao == "") {
-            resposta.write(`<div m-2 class="alert alert-danger" role="alert">
-                                    Por favor, informe a razão social.
-                                </div>`);
+         resposta.write(`
+            <label for="descricao">Descrição do Produto:</label>
+            <input type="text" id="descricao" name="descricao" value="${descricao}" required>`);
+         if (descricao= "") {
+            resposta.write(`
+                <div class="alert alert-danger" role="alert">
+                    Por favor, informe a descrição do produto.
+                </div>
+            `);
         }
-        resposta.write(`<label for="nome_fantasia">Nome Fantasia:</label>
-            <input type="text" id="nome_fantasia" name="nome_fantasia" placeholder="Loja do 1,99" value="${precoCusto}" required>`);
-        if (precoCusto == "") {
-            resposta.write(`<div m-2 class="alert alert-danger" role="alert">
-                                    Por favor, informe o nome fantasia.
-                                </div>`);
+         resposta.write(`
+            <label for="precoCusto">Preço de Custo:</label>
+            <input type="number" id="precoCusto" name="precoCusto" value="${precoCusto}" step="0.01" required>`);
+         if (precoCusto="") {
+            resposta.write(`
+                <div class="alert alert-danger" role="alert">
+                    Por favor, informe o preço de custo.
+                </div>
+            `);
         }
-        resposta.write(`<label for="endereco">Endereço:</label>
-            <input type="text" id="endereco" name="endereco" value="${precoVenda}" required>`);
-        if (precoVenda == "") {
-            resposta.write(`<div m-2 class="alert alert-danger" role="alert">
-                                    Por favor, informe o endereço.
-                                </div>`);
+         resposta.write(`
+            <label for="precoVenda">Preço de Venda:</label>
+            <input type="number" id="precoVenda" name="precoVenda" value="${precoVenda}" step="0.01" required>`);
+         if (precoVenda="") {
+            resposta.write(`
+                <div class="alert alert-danger" role="alert">
+                    Por favor, informe o preço de venda.
+                </div>
+            `);
         }
-        resposta.write(`<label for="cidade">Cidade:</label>
-            <input type="text" id="cidade" name="cidade" value="${validade}" required>`);
-        if (validade == "") {
-            resposta.write(`<div m-2 class="alert alert-danger" role="alert">
-                                    Por favor, informe a cidade.
-                                </div>`);
+         resposta.write(`
+            <label for="validade">Data de Validade:</label>
+            <input type="date" id="validade" name="validade" value="${validade}" required>`);
+         if (validade="") {
+            resposta.write(`
+                <div class="alert alert-danger" role="alert">
+                    Por favor, informe a data de validade.
+                </div>
+            `);
         }
-        resposta.write(`<label for="uf">UF:</label>
-            <input type="text" id="uf" name="uf" maxlength="2" value="${estoque}" required>`);
-        if (estoque == "") {
-            resposta.write(`<div m-2 class="alert alert-danger" role="alert">
-                                    Por favor, informe sua região.
-                                </div>`);
+         resposta.write(`
+            <label for="estoque">Quantidade em Estoque:</label>
+            <input type="number" id="estoque" name="estoque" value="${estoque}" required>`);
+         if (estoque="") {
+            resposta.write(`
+                <div class="alert alert-danger" role="alert">
+                    Por favor, informe a quantidade em estoque.
+                </div>
+            `);
         }
-        resposta.write(`<label for="cep">CEP:</label>
-            <input type="text" id="cep" name="cep" value="${fabricante}" required>`);
-        if (fabricante == "") {
-            resposta.write(`<div m-2 class="alert alert-danger" role="alert">
-                                    Por favor, informe o cep.
-                                </div>`);
+         resposta.write(`
+            <label for="fabricante">Nome do Fabricante:</label>
+            <input type="text" id="fabricante" name="fabricante" value="${fabricante}" required>`);
+         if (fabricante="") {
+            resposta.write(`
+                <div class="alert alert-danger" role="alert">
+                    Por favor, informe o nome do fabricante.
+                </div>
+            `);
         }
-        resposta.write(` 
+         resposta.write(`
             <div class="buttons">
-                 <button type="submit">Cadastrar</button>
-                 <button type="button" onclick="history.back()">Voltar</button>
-             </div>
-             </form>
-             </body>
-             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-            crossorigin="anonymous"></script>
-             </html>`);
+                <button type="submit">Cadastrar Produto</button>
+                <button type="button" onclick="window.history.back()">Voltar</button>
+            </div>
+            </form>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        </body>
+        </html>`);
         resposta.end();
     }
+
 }
 
-function autenticarUsuario(requisicao, resposta){
-    const usuario = requisicao.body.usuario;
-    const senha = requisicao.body.senha;
-    if (usuario == 'admin' && senha == '123'){
+ function autenticarUsuario(requisicao, resposta){
+     const usuario = requisicao.body.usuario;
+     const senha = requisicao.body.senha;
+     if (usuario == 'admin' && senha == '123'){
         requisicao.session.usuarioAutenticado = true;
         resposta.cookie('dataUltimoAcesso', new Date().toLocaleString(),{
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 30
         });
-        resposta.redirect('/');
+         resposta.redirect('/');
     }
-    else{
+     else{
         resposta.write(`
         <!DOCTYPE html>
         <html>
@@ -159,7 +182,7 @@ function autenticarUsuario(requisicao, resposta){
             <p>Usuário ou senha inválidos!</p>
             <a href="/login.html">Voltar</a>
     `);
-    if (requisicao.cookies.dataUltimoAcesso) {
+     if (requisicao.cookies.dataUltimoAcesso) {
         resposta.write(`
             <p>
                 Seu último acesso foi em ${requisicao.cookies.dataUltimoAcesso}
@@ -167,11 +190,11 @@ function autenticarUsuario(requisicao, resposta){
         `);
     }
 
-    resposta.write(`
+     resposta.write(`
         </body>
         </html>
-    `);
-    resposta.end();
+     `);
+     resposta.end();
     }
 }
 app.post('/login', autenticarUsuario);
@@ -228,35 +251,24 @@ app.get('/listarUsuarios', usuarioEstaAutenticado, (req, resp) => {
                 <td>${listaUsuarios[i].estoque}</td>
                 <td>${listaUsuarios[i].fabricante}</td>
             </tr>`;
-    }
+     }
 
-    conteudoResposta += `
+     conteudoResposta += `
         </table>
         <a href="/">Voltar</a>`;
     
-    if (req.cookies.dataUltimoAcesso) {
+     if (req.cookies.dataUltimoAcesso) {
         conteudoResposta += `
             <p>Seu último acesso foi em ${req.cookies.dataUltimoAcesso}</p>`;
     }
 
-    conteudoResposta += `
+     conteudoResposta += `
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
     </html>`;
 
     resp.send(conteudoResposta);
 });
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(porta, host, () => {
     console.log(`Servidor rodando em http://${host}:${porta}`);
